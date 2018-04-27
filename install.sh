7ce5b826a7dd898d6965c4561879c7e615325a01
saito <test.mail.3387@gmail.com>
2018-04-27 13:46:00 +0900

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
+docker exec dockerappengine_php_1 \
+bash -c "mkdir /var/www/html/wordpress;"
+
 docker exec -it dockerappengine_php_1 \
-composer install
+composer update
 
 docker exec dockerappengine_php_1 \
-bash -c "cd /var/www/html/wordpress; wp theme activate default_theme --allow-root;"
+bash -c "
+cd /var/www/html/wordpress;
+wp config create --dbname=test --dbuser=root --dbpass=pass --dbhost=mysql --allow-root;
+wp core install --url=example.com --title=Example --admin_user=supervisor --admin_password=strongpassword --admin_email=info@example.com --allow-root;
+wp theme activate default_theme --allow-root;"
 
-ln -snf $PWD/wordpress/wp-content $PWD/wp-content
-ln -snf $PWD/wordpress/wp-config.php $PWD/wp-config.php
+dir=$(cd $(dirname $0) && pwd)
+echo $dir/wordpress/wp-content
+ln -snf $dir/wordpress/wp-content $dir/wp-content
+ln -snf $dir/wordpress/wp-config.php $dir/wp-config.php
